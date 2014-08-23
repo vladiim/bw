3e64b85666538ac36608b1666ce2f23f535be10e
Vlad Ivanovic <vladiim@me.com>
2014-08-23 16:28:27 +1000

    Database connected

--- config/database.rb
+++ config/database.rb
@@ -2,6 +2,6 @@ Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("postgres://localhost/bw_development", :loggers => [logger])
-  when :production  then Sequel.connect("postgres://localhost/bw_production",  :loggers => [logger])
+  when :production  then Sequel.connect(ENV['PRODUCTION_DB'],  :loggers => [logger])
   when :test        then Sequel.connect("postgres://localhost/bw_test",        :loggers => [logger])
 end
