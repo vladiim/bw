550fcf66e4f49c345f458b9a8051216d756651e9
Vlad Ivanovic <vladiim@me.com>
2014-08-31 21:30:35 +1000

    Article#find sets up saved_image and saved_hero_image

--- models/article.rb
+++ models/article.rb
@@ -1,19 +1,18 @@
 class Article < Sequel::Model
 
   attr_accessor :image, :hero_image
-
   def initialize(attrs={})
     @image = Image.new
     @hero_image = HeroImage.new
     super(attrs)
   end
 
-  def image_title=(title)
-    image.title = title
-  end
-
-  def image_file=(file)
-    image.file = file
+  attr_accessor :saved_image, :saved_hero_image
+  def self.find(article_id)
+    article                  = Article[article_id]
+    article.saved_hero_image = HeroImage.first({ article_id: article.id })
+    article.saved_image      = Image[article.saved_hero_image.image_id]
+    article
   end
 
   def save(super_proc = ->{ super })
@@ -24,5 +23,22 @@ class Article < Sequel::Model
     hero_image.save
   end
 
+  def image_title=(title)
+    image.title = title
+  end
+
+  def image_file=(file)
+    image.file = file
+  end
+
+  def image_url
+    @saved_image ? @saved_image.values[:url] : ''
+  end
+
+  def image_title
+    @saved_image ? @saved_image.values[:title] : ''
+  end
+
   # add image finder for when editing an article
+  # destroy hero image on destroy
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -28,8 +28,69 @@ RSpec.describe Article do
     end
   end
 
+  context 'saved article' do
+    describe '.find' do
+      let(:article)          { Article.find(1) }
+      let(:saved_article)    { OpenStruct.new }
+      let(:saved_hero_image) { OpenStruct.new }
+      let(:saved_image)      { OpenStruct.new }
+
+      before do
+        allow(Article).to receive(:[]).with(1) { saved_article }
+        saved_article.id = 2
+        allow(HeroImage).to receive(:first).with({ article_id: 2 }) { saved_hero_image }
+        saved_hero_image.image_id = 3
+        allow(Image).to receive(:[]).with(3) { saved_image }
+      end
+
+      it 'finds the article' do
+        expect(article).to eq saved_article
+      end
+
+      it 'finds and sets the saved_hero_image' do
+        expect(article.saved_hero_image).to eq saved_hero_image
+      end
+
+      it 'finds and sets the saved_image' do
+        expect(article.saved_image).to eq saved_image
+      end
+    end
+  end
+
+  describe '#image_url' do
+    context 'with saved_image' do
+      before { article.saved_image = OpenStruct.new(values: { url: 'IMAGE URL' }) }
+
+      it "returns the image's url" do
+        expect(article.image_url).to eq 'IMAGE URL'
+      end
+    end
+
+    context 'without saved_image' do
+      it 'returns an empty string' do
+        expect(article.image_url).to eq ''
+      end
+    end
+  end
+
+  describe '#image_title' do
+    context 'with saved_image' do
+      before { article.saved_image = OpenStruct.new(values: { title: 'IMAGE TITLE' }) }
+
+      it "returns the image's title" do
+        expect(article.image_title).to eq 'IMAGE TITLE'
+      end
+    end
+
+    context 'without saved_image' do
+      it 'returns an empty string' do
+        expect(article.image_title).to eq ''
+      end
+    end
+  end
+
   describe '#save' do
-    context 'image params saved', focus: true do
+    context 'image params saved' do
       let(:super_proc) { ->{ article.values[:id] = 'ARTICLE ID' } }
       let(:result)     { article.save(super_proc) }
 
